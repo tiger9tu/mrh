2c77d4c16e32d54f572d5799bc30a6f9eb406c0f
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-07-11 09:40:11 -0500

    Fixing some attr errors

--- my_pyscf/pbc/mcpdft/kmcpdft.py
+++ my_pyscf/pbc/mcpdft/kmcpdft.py
@@ -3,7 +3,7 @@ import numpy as np
 from pyscf.mcpdft.mcpdft import _PDFT
 from pyscf.pbc.dft import gen_grid as pbc_gen_grid
 
-from mrh.my_pyscf.pbc.mcpdft.otfnalperiodic import get_pbc_transfnal
+from mrh.my_pyscf.pbc.mcpdft.otfnalperiodic import get_pbc_otfnal
 
 '''
 Author: Bhavnesh Jangid
@@ -16,6 +16,7 @@ class _kMCPDFT(_PDFT):
     This class is making sure, the functionalities which are not 
     compatible with periodic systems are throwing NotImplementedError. 
     '''
+
     def _init_ot_grids(self, my_ot, grids_attr=None):
         '''
         Initialization of on-top functional and grids for periodic systems.
@@ -27,7 +28,7 @@ class _kMCPDFT(_PDFT):
 
         if isinstance(my_ot, (str, np.bytes_)):
             # Note: I have changed the input arg. for below function.
-            self.otfnal = get_pbc_transfnal(self._mc_class, my_ot)
+            self.otfnal = get_pbc_otfnal(self._scf, my_ot)
         else:
             self.otfnal = my_ot
 
@@ -39,7 +40,7 @@ class _kMCPDFT(_PDFT):
         if isinstance(old_grids, pbc_grid_types):
             self.otfnal.grids = old_grids
         else:
-            self.otfnal.grids = pbc_gen_grid.BeckeGrids(self.mol)
+            self.otfnal.grids = pbc_gen_grid.BeckeGrids(self.cell,)
 
         self.otfnal.grids.__dict__.update(grids_attr)
 
@@ -53,13 +54,16 @@ class _kMCPDFT(_PDFT):
         raise NotImplementedError(f"StateAverageMix not available for {method}")
 
 
-def get_mcpdft_child_class(mc, ot, **kwargs):
-    mc_doc = (mc.__class__.__doc__ or 'No docstring for MC-SCF parent method')
+def get_mcpdft_child_class(kmc, ot, **kwargs):
+    mc_doc = (kmc.__class__.__doc__ or 'No docstring for MC-SCF parent method')
 
-    class PDFT(_kMCPDFT, mc.__class__):
+    class PDFT(_kMCPDFT, kmc.__class__):
         __doc__ = mc_doc + '\n\n' + _kMCPDFT.__doc__
-        _mc_class = mc.__class__
+        _mc_class = kmc.__class__
 
+        # MC-PDFT object requires mol object in ot.reset functions
+        _mc_class.mol = kmc._scf.cell.to_mol()
+        
         def compute_pdft_energy_(self, mo_coeff=None, ci=None, ot=None, otxc=None,
                                  grids_level=None, grids_attr=None, dump_chk=False, **kwargs):
 
@@ -70,8 +74,8 @@ def get_mcpdft_child_class(mc, ot, **kwargs):
             return _kMCPDFT.compute_pdft_energy_(self, mo_coeff=mo_coeff, ci=ci, ot=ot, otxc=otxc,
                     grids_level=grids_level, grids_attr=grids_attr, dump_chk=False, **kwargs)
      
-    pdft = PDFT(mc._scf, mc.ncas_sub, mc.nelecas_sub, my_ot=ot, **kwargs)
+    pdft = PDFT(kmc._scf, kmc.ncas, kmc.nelecas, my_ot=ot, **kwargs)
     _keys = pdft._keys.copy()
-    pdft.__dict__.update(mc.__dict__)
+    pdft.__dict__.update(kmc.__dict__)
     pdft._keys = pdft._keys.union(_keys)
     return pdft
